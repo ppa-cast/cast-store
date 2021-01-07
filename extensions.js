a18e8672d55f86eccbf73d474b2e3f572bc1beac
PPA <56297840+ppa-cast@users.noreply.github.com>
2021-01-07 20:13:18 +0100

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -194,6 +194,7 @@ var extensionsDirectory = {
     "com.castsoftware.imaging.etl"                                : "platform",
     "com.castsoftware.imaging.docker"                             : "platform",
     "com.castsoftware.uc.demeter"                                 : "platform",
+    "com.castsoftware.aip.extendproxy"                            : "platform",
     
     "com.castsoftware.aip.datamart"                               : "reporting",
     "com.castsoftware.labs.templates.for.report.generator"        : "reporting",
